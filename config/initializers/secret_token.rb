e01507ca5e68ae216013518494fcb847694cc44d
Keanu Lee <keanulee517@gmail.com>
2013-05-16 23:11:26 -0400

    Empty Rails repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CvHackathon::Application.config.secret_token = 'c8c684e0c55a812fef06d7a46709d779dd0c8c63a25a7d068c3ada03b11a5ff7e4e35af4693c46156fac281169dacf2040428afee8ccdf0ceea5fee94267ce04'
